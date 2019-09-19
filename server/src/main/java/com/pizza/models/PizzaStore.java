ded430178a803ff26a711b72c1bf201a7a4b949c
Heinrich Chen <heinricc@uci.edu>
2019-09-19 14:04:25 -0400

    Updated PizzaStore class - changed @Column sql names to camel casing - indented it properly - added constructors (empty and with values) - added toString method - add getter and setter for zipCode
    Added PizzaStoreController class
    
    Added PizzaStoreRepository

--- server/src/main/java/com/pizza/models/PizzaStore.java
+++ server/src/main/java/com/pizza/models/PizzaStore.java
@@ -3,6 +3,12 @@ package com.pizza.models;
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * Database entity for PizzaStores. Has many-to-many relationship with
@@ -10,78 +16,107 @@ import java.util.Set;
  */
 @Entity
 @Table(name = "PizzaStores")
-public class PizzaStore {
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  @Column(name = "StoreID")
-  private Long storeId;
+public class PizzaStore 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "storeID")
+	private Long storeId;
 
-  @Column(name = "StoreName")
-  private String storeName;
+	@Column(name = "storeName")
+	private String storeName;
 
-  @Column(name = "city")
-  private String city;
+	@Column(name = "city")
+	private String city;
 
-  @Column(name = "state")
-  private String state;
+	@Column(name = "state")
+	private String state;
 
-  @Column(name = "ZipCode")
-  private String zipCode;
+	@Column(name = "zipCode")
+	private String zipCode;
 
-  @ManyToMany(fetch = FetchType.LAZY,
-    cascade = {
-      CascadeType.PERSIST,
-    })
-  @JoinTable(name = "PizzaStores_has_FoodItems",
-    joinColumns = { @JoinColumn(name = "PizzaStoreID") },
-    inverseJoinColumns = { @JoinColumn(name = "FoodItemID") })
-  private Set<FoodItem> foodItems = new HashSet<>();
+	@ManyToMany(fetch = FetchType.LAZY,
+			cascade = {
+					CascadeType.PERSIST,
+	})
+	@JoinTable(name = "PizzaStores_has_FoodItems",
+	joinColumns = { @JoinColumn(name = "PizzaStoreID") },
+	inverseJoinColumns = { @JoinColumn(name = "FoodItemID") })
+	private Set<FoodItem> foodItems = new HashSet<>();
 
-  public String getStoreName() {
-    return storeName;
-  }
+	public PizzaStore() {
+		super();
+	}
 
-  public void setStoreName(String storeName) {
-    this.storeName = storeName;
-  }
+	public PizzaStore(String storeName, String city, String state, String zipCode, Set<FoodItem> foodItems) 
+	{
+		super();
+		this.storeName = storeName;
+		this.city = city;
+		this.state = state;
+		this.zipCode = zipCode;
+		this.foodItems = foodItems;
+	}
 
-  public Long getStoreID() {
-      return storeId;
-  }
+	public String getStoreName() {
+		return storeName;
+	}
 
-  public void setStoreID(Long storeID) {
-    storeId = storeID;
-  }
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
 
-  public String getCity() {
-    return city;
-  }
+	public Long getStoreID() {
+		return storeId;
+	}
 
-  public void setCity(String city) {
-    this.city = city;
-  }
+	public void setStoreID(Long storeID) {
+		storeId = storeID;
+	}
 
-  public String getState() {
-    return state;
-  }
+	public String getCity() {
+		return city;
+	}
 
-  public void setState(String state) {
-    this.state = state;
-  }
+	public void setCity(String city) {
+		this.city = city;
+	}
 
-  public Long getStoreId() {
-    return storeId;
-  }
+	public String getState() {
+		return state;
+	}
 
-  public void setStoreId(Long storeId) {
-    this.storeId = storeId;
-  }
+	public void setState(String state) {
+		this.state = state;
+	}
 
-  public Set<FoodItem> getFoodItems() {
-    return foodItems;
-  }
+	public Long getStoreId() {
+		return storeId;
+	}
 
-  public void setFoodItems(Set<FoodItem> foodItems) {
-    this.foodItems = foodItems;
-  }
+	public void setStoreId(Long storeId) {
+		this.storeId = storeId;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public Set<FoodItem> getFoodItems() {
+		return foodItems;
+	}
+
+	public void setFoodItems(Set<FoodItem> foodItems) {
+		this.foodItems = foodItems;
+	}
+
+	@Override
+	public String toString() {
+		return "PizzaStore [storeId=" + storeId + ", storeName=" + storeName + ", city=" + city + ", state=" + state
+				+ ", zipCode=" + zipCode + ", foodItems=" + foodItems + "]";
+	}
 }
