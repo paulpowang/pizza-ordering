e794a18a1de2562ae67753e15d3133edfecaf9c0
jchong1 <jocchong@ucsc.edu>
2019-09-19 22:15:23 -0400

    food item class

--- src/app/models/store.ts
+++ src/app/models/store.ts
@@ -1,7 +1,10 @@
+import { FoodItem } from './food-item';
+
 export class Store {
-    storeId: number;
-    storeName: string;
-    city: string;
-    state: string;
-    zipCode: string;
+  storeId: number;
+  storeName: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  foodItems: Set<FoodItem>;
 }
