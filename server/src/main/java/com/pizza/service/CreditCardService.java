643659ef5cb8855885b63449b96543d34a4e4d67
PoHung Wang <wang.po.hung@gmail.com>
2019-09-22 08:18:44 -0700

    Created CreditCardService for CreditCardController.

---
+++ server/src/main/java/com/pizza/service/CreditCardService.java
@@ -0,0 +1,61 @@
+package com.pizza.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pizza.repositories.CreditCardRepository;
+import com.pizza.models.CreditCardDetail;
+
+@Service
+public class CreditCardService {
+	
+	@Autowired
+	private CreditCardRepository repository;
+	
+	List<CreditCardDetail> list = new ArrayList<>();
+	
+	// return all credit card
+	public List<CreditCardDetail> getAllTheCreditCards(){
+		List<CreditCardDetail> list = new ArrayList<>();
+		repository.findAll().forEach(list::add);
+		return list;
+	}
+	
+	
+	// return credit card by id
+	public CreditCardDetail getCreditCardDetail(long id) {
+		Optional<CreditCardDetail> optionalCreditCard = repository.findById(id);
+		
+		if(optionalCreditCard.isPresent()) {
+			return optionalCreditCard.get();
+		}
+		return null;
+		
+		
+	}
+	
+	
+	// save the credit card
+	public void saveCreditCardDetail(CreditCardDetail card) {
+		repository.save(card);
+	}
+	
+	// update the credit card
+	public void updateCreditCardDetail(long id,CreditCardDetail card) {
+		Optional<CreditCardDetail> optionalCard =  repository.findById(id);
+		CreditCardDetail card1 = optionalCard.get();
+		card1.setCreditCardNumber(card.getCreditCardNumber());
+		repository.save(card1);
+	}
+	
+	// remove the credit card by id
+	public void deleteCreditCardDetail(long id) {
+		repository.deleteById(id);;
+	}
+	
+
+}
