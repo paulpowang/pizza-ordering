8a4854664fada611fa0e133a1acf25fe4cabf54d
yumengge <yge@berkeley.edu>
2019-09-24 09:18:00 -0400

    UI

--- server/src/main/java/com/pizza/ServerApplication.java
+++ server/src/main/java/com/pizza/ServerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class ServerApplication {
 
   public static void main(String[] args) {
+    System.out.println(org.hibernate.Version.getVersionString());
     SpringApplication.run(ServerApplication.class, args);
   }
 
