b6b509bdfbb249840c10fadd05ae1e760cdd34d3
jchong1 <jocchong@ucsc.edu>
2019-09-25 13:19:00 -0400

    submitting order works now

--- src/app/checkout/checkout-view/checkout-view.component.ts
+++ src/app/checkout/checkout-view/checkout-view.component.ts
@@ -11,8 +11,6 @@ import { CreditCardDetail } from '../../models/credit-card-detail';
 import { ShoppingCart } from '../../models/shopping-cart';
 import { ShoppingCartItem } from '../../models/shopping-cart-item';
 
-
-
 export interface TempItem {
   name: string;
   price: number;
@@ -21,109 +19,95 @@ export interface TempItem {
 }
 
 const test_data: TempItem[] = [
-  {name: 'Pineapple Pizza', price: 5.00, qty: 1, total: 5},
-  {name: 'Coke',            price: 1.00, qty: 2, total: 2},
-  {name: 'Peperoni Pizza',  price: 5.00, qty: 1, total: 5},
+  { name: 'Pineapple Pizza', price: 5.0, qty: 1, total: 5 },
+  { name: 'Coke', price: 1.0, qty: 2, total: 2 },
+  { name: 'Peperoni Pizza', price: 5.0, qty: 1, total: 5 },
 ];
-
 
 @Component({
   selector: 'app-checkout-view',
   templateUrl: './checkout-view.component.html',
-  styleUrls: ['./checkout-view.component.css']
+  styleUrls: ['./checkout-view.component.css'],
 })
 export class CheckoutViewComponent implements OnInit {
-
-  
-
   shipments: Array<ShipmentDetail>;
   creditcards: Array<CreditCardDetail>;
   shoppingCart: ShoppingCart;
 
-
-  
   // cardId & shipId use for locate radio option
-  cardId:number;
-  shipId:number;
+  cardId: number;
+  shipId: number;
 
   //user info
   userId: string;
-  email:string;
+  email: string;
 
   //shoppingCartItems: List<ShoppingCartItem>
-shoppingCartItems;
-displayedColumns: string[] = ['name', 'price', 'qty', 'total'];
+  shoppingCartItems;
+  displayedColumns: string[] = ['name', 'price', 'qty', 'total'];
 
   // total for shopping cart
-  total:number;
+  total: number;
 
-  constructor(private router: Router, 
-              private creditcardService: CreditcardService,
-              private shipmentService: ShipmentService,
-              private service: OrderService) { }
+  constructor(
+    private router: Router,
+    private creditcardService: CreditcardService,
+    private shipmentService: ShipmentService,
+    private service: OrderService
+  ) {}
 
   ngOnInit() {
-    
-    
     this.shipments = this.service.shipmentDetails;
-    
-    this.creditcards = this.service.creditCardDetails;
-    
 
-    
-    this.shoppingCartItems = this.service.shoppingCart.shoppingCartItems.map(shoppingCartItem =>{
-      return {name: shoppingCartItem.foodItem.name, 
-              price: shoppingCartItem.foodItem.price, 
-              qty: shoppingCartItem.quantity, 
-              total: shoppingCartItem.foodItem.price * shoppingCartItem.quantity}
+    this.creditcards = this.service.creditCardDetails;
+
+    this.shoppingCartItems = this.service.shoppingCart.shoppingCartItems.map(shoppingCartItem => {
+      return {
+        name: shoppingCartItem.foodItem.name,
+        price: shoppingCartItem.foodItem.price,
+        qty: shoppingCartItem.quantity,
+        total: shoppingCartItem.foodItem.price * shoppingCartItem.quantity,
+      };
     });
-    
-    
 
     this.total = this.service.shoppingCart.getTotalCost();
 
     this.email = this.service.getUserId();
-    this.userId = this.email.split("@")[0];
+    this.userId = this.email.split('@')[0];
   }
 
-  addCreditcard(){
+  addCreditcard() {
     this.router.navigate(['/checkout/creditcard/add']);
   }
 
-  editCreditcard(id:number){
+  editCreditcard(id: number) {
     this.router.navigate(['/checkout/creditcard/edit/' + id]);
   }
 
-  addShipment(){
+  addShipment() {
     this.router.navigate(['/checkout/shipment/add']);
   }
 
-  editShipment(id:number){
+  editShipment(id: number) {
     this.router.navigate(['/checkout/shipment/edit/' + id]);
   }
 
-
-  back(){
+  back() {
     this.router.navigate(['']);
-
   }
 
-  toSummary(){
-    console.log("ship" + this.shipId);
-    console.log("card" + this.cardId);
+  toSummary() {
+    console.log('ship' + this.shipId);
+    console.log('card' + this.cardId);
 
-    if(this.shipId && this.cardId){
-
+    if (this.shipId && this.cardId) {
       this.service.creditCardId = this.cardId;
-    this.service.shippingId = this.shipId;
-    this.creditcardService.setCardId(this.cardId);
-    this.shipmentService.setShipId(this.shipId);
-    this.router.navigate(['/summary']);
-    }else{
-      alert("please select payment and shipping address");
+      this.service.shippingId = this.shipId;
+      this.creditcardService.setCardId(this.cardId);
+      this.shipmentService.setShipId(this.shipId);
+      this.router.navigate(['/summary']);
+    } else {
+      alert('please select payment and shipping address');
     }
-    
-
   }
-
 }
--- src/app/summary-view/summary-view.component.ts
+++ src/app/summary-view/summary-view.component.ts
@@ -10,7 +10,6 @@ import { OrderService } from '../services/order.service';
 import { ShoppingCartItem } from '../models/shopping-cart-item';
 import { ShoppingCart } from '../models/shopping-cart';
 
-
 export interface TempItem {
   name: string;
   price: number;
@@ -19,83 +18,78 @@ export interface TempItem {
 }
 
 const test_data: TempItem[] = [
-  {name: 'Pineapple Pizza', price: 5.00, qty: 1, total: 5},
-  {name: 'Coke',            price: 1.00, qty: 2, total: 2},
-  {name: 'Peperoni Pizza',  price: 5.00, qty: 1, total: 5},
+  { name: 'Pineapple Pizza', price: 5.0, qty: 1, total: 5 },
+  { name: 'Coke', price: 1.0, qty: 2, total: 2 },
+  { name: 'Peperoni Pizza', price: 5.0, qty: 1, total: 5 },
 ];
-
 
 @Component({
   selector: 'app-summary-view',
   templateUrl: './summary-view.component.html',
-  styleUrls: ['./summary-view.component.css']
+  styleUrls: ['./summary-view.component.css'],
 })
-
-
 export class SummaryViewComponent implements OnInit {
+  //userCredential: UserCredential
+  email: string;
+  username: string;
 
+  //shoppingCartItems: List<ShoppingCartItem>
+  shoppingCartItems;
+  displayedColumns: string[] = ['name', 'price', 'qty', 'total'];
 
-//userCredential: UserCredential
-email: string;
-username:string;
-
-//shoppingCartItems: List<ShoppingCartItem>
-shoppingCartItems;
-displayedColumns: string[] = ['name', 'price', 'qty', 'total'];
-
-//calculate this somehow
-total: number;
-
-
+  //calculate this somehow
+  total: number;
 
   shipment: Shipment;
   creditcard: Creditcard;
   shoppingCart: ShoppingCart;
-  
-  constructor(private router: Router, 
+
+  constructor(
+    private router: Router,
     private creditcardService: CreditcardService,
     private shipmentService: ShipmentService,
     private orderService: OrderService,
-    private location: Location) { }
+    private location: Location
+  ) {}
 
   ngOnInit() {
-
-
     this.creditcardService.getCreditcardByCardId().subscribe(creditcard => {
       this.creditcard = creditcard;
     });
     this.shipmentService.getShipmentByShipId().subscribe(shipment => {
       this.shipment = shipment;
-
     });
 
-    
-
-    this.shoppingCartItems = this.orderService.shoppingCart.shoppingCartItems.map(shoppingCartItem =>{
-      return {name: shoppingCartItem.foodItem.name, 
-              price: shoppingCartItem.foodItem.price, 
-              qty: shoppingCartItem.quantity, 
-              total: shoppingCartItem.foodItem.price * shoppingCartItem.quantity}
-    });
-    
-    
+    this.shoppingCartItems = this.orderService.shoppingCart.shoppingCartItems.map(
+      shoppingCartItem => {
+        return {
+          name: shoppingCartItem.foodItem.name,
+          price: shoppingCartItem.foodItem.price,
+          qty: shoppingCartItem.quantity,
+          total: shoppingCartItem.foodItem.price * shoppingCartItem.quantity,
+        };
+      }
+    );
 
     this.total = this.orderService.shoppingCart.getTotalCost();
 
     this.email = this.orderService.getUserId();
-    this.username = this.email.split("@")[0];
-
-    
-
+    this.username = this.email.split('@')[0];
   }
 
-  back(){
+  back() {
     this.location.back();
   }
 
-  toPlaceOrder(){
-    this.orderService.save();
-    this.router.navigate(['/thankyou']);
+  toPlaceOrder() {
+    console.log(this.orderService);
+    this.orderService.save().subscribe(
+      () => {
+        this.router.navigate(['/thankyou']);
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
-
 }
